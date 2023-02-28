443f4d70a025d859b96cbb64723948b92ddcf49c
qweqweqweqwewqe124r12r21 <126561621+qweqweqweqwewqe124r12r21@users.noreply.github.com>
2023-02-28 10:06:31 +0000

    Update jiggaboo.lua

--- jiggaboo.lua
+++ jiggaboo.lua
@@ -1,10 +1,10 @@
-local gvar = '1231321532523523532' --/ change this every time
+local gvar = '1111111111' --/ change this every time
 
 if not _G[gvar] then
 	_G[gvar] = true
 
 	--/ Lua to load here
 	
- 	print('Test')
+ 	print('der')
   
 end
